881449a6804c125f629dcc104c14b57e4aa5cb48
marissacraig <135668035+marissacraig@users.noreply.github.com>
2023-07-14 00:35:47 -0700

    created quiz questions

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
-
+var question = document.querySelector("#question");
+var answers = document.querySelector("#answers");
 var score = document.querySelector(".win");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
@@ -12,30 +13,45 @@ var timerCount;
 // Questions that user will be asked and possible answers
 // The correct answer is located at the index number listed after answers
 var allQuestions = [{
-  q: "",
-  a: ["","","","",0],
+  q: "Which of these characters is used to call on an element with a class selector?",
+  a: [".",
+      "#",
+      "*",
+      "$",
+      0],
 },
 {
-  q: "",
-  a: ["","","","",1]
+  q: "Which of these data types defines 'a series of characters surrounded by quotes'?",
+  a: ["boolean",
+      "string",
+      "array",
+      "undefined",
+      1]
 },
 {
-  q: "",
-  a: ["","","","",3]
+  q: "CSS is used to: ",
+  a: ["structure a webpage",
+      "create interactive effects on a webpage",
+      "reset the styling of HTML elements to a consistent baseline",
+      "style the HTML elements of a webpage",
+      3]
 },
 {
-  q: "",
-  a: ["","","","",2]
+  q: "What number is used to tell a for loop how many times to execute over an array?",
+  a: ["The letter 'i'",
+      "The number of variables",
+      "The length of the array",
+      "The number of objects",
+      2]
 },
 {
-  q: "",
-  a: ["","","","",0]
+  q: "Which of these words is not an Element on the DOM Tree?",
+  a: ["html","body","h1","a",0]
 }]
 
 // The init function is called when the page loads 
 function init() {
   getScores();
-  
 }
 
 // The startQuiz function is called when the start button is clicked
@@ -49,7 +65,7 @@ function startQuiz() {
   startTimer()
 }
 
-// The winGame function is called when the win condition is met
+// The endQuiz function is called when the end condition is met
 function endQuiz() {
   wordBlank.textContent = "Your score 🏆 ";
   newScore++
@@ -82,21 +98,24 @@ function startTimer() {
   }, 1000);
 }
 
-// Picks a random question
+// Picks a question
 function loadQuestion(currentQuestion) {
-  var question = document.querySelector("#question");
-  var answers = document.querySelector("#answers");
-  
-  question.textContent = allQuestions[currentQuestion].q;
-  answers.textContent = allQuestions[currentQuestion].a;
-}
+  console.log(currentQuestion);
+  var nextQuestion = document.createElement('h1');
+  var nextAnswers = document.createElement('p');  
+  nextQuestion.textContent = allQuestions[currentQuestion].q;
+  nextAnswers.textContent = allQuestions[currentQuestion].a;
+  question.append(nextQuestion);
+  answers.append(nextAnswers);
 
-// Attach event listener to document to listen for key event
-document.addEventListener("click", checkAnswer(i, answers)) 
+
+}
 
 
 // Tests if guessed answer is correct
 function checkAnswer(i, arr) {
+// Attach event listener to document to listen for key event
+document.addEventListener("click", checkAnswer(i, answers)) 
   // If the count is zero, exit function
   if (timerCount === 0) {
     return;
@@ -110,11 +129,19 @@ function checkAnswer(i, arr) {
   for (var i = 0; i < answers.length -1; i += 1) {
     if (chosenAnswer[i] === correctAnswer) {
       chosenAnswer = true;
+    } else {
+
     }
   }
 
    // -- I answer a question; I am presented with another question
-
+   if (current < Object.keys(allQuestions).length -1) {
+    current += 1;
+    loadQuestion(current);
+  } else {
+    question.innerHTML = 'Done';
+    answers.innerHTML = '';
+  }
    return 
   }
 };
